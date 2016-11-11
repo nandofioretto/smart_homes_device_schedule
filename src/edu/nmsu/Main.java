09d9d07e857ab84c79f042e1bdf35c6871c138f1
Ferdinando Fioretto <fioretto@umich.edu>
2016-11-11 10:38:34 -0500

    no message

--- src/edu/nmsu/Main.java
+++ src/edu/nmsu/Main.java
@@ -44,7 +44,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        for (int i=0; i < 5; i++) {
+        for (int i=0; i < 100; i++) {
             generateSHDSInstances();
             execute();
         }
